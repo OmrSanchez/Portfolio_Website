c770d41868d8a499917aaebe7f03f7d95b854d41
OmrSanchez <omar.sanchez008@gmail.com>
2025-01-07 22:10:53 -0500

    Create contact me form and send email.

--- send_email.py
+++ send_email.py
@@ -1,23 +1,16 @@
 import smtplib, ssl
 
-# from pages.Contact_Me import message
+def send_email(message):
+        host = "smtp.gmail.com"
+        port = 465
 
-host = "smtp.gmail.com"
-port = 465
+        username = "osanchez.python@gmail.com"
+        password = ""
 
+        receiver = "osanchez.python@gmail.com"
+        context = ssl.create_default_context()
 
-username = "osanchez.python@gmail.com"
-password = "dhmu kggi jrzh azje"
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+                server.login(username, password)
+                server.sendmail(username, receiver, message)
 
-receiver = "osanchez.python@gmail.com"
-context = ssl.create_default_context()
-
-message = '''\
-Subject: Hi!
-How are you?
-Bye!
-'''
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
