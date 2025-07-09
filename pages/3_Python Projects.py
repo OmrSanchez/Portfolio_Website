4bb95b5de82492a8b4cdd7514d5a689e60592461
osanchez <omar.sanchez008@gmail.com>
2025-07-09 11:48:39 -0400

    Working

--- pages/3_Python Projects.py
+++ pages/3_Python Projects.py
@@ -10,15 +10,13 @@ st.set_page_config(
 
 csv_path = Path(__file__).parent.parent / 'python_projects_data.csv'
 
-
-
 st.header("Python Projects")
 
 df = pandas.read_csv(csv_path, sep=";")
 col3, col4 = st.columns(2)
 
 with col3:
-	for index, row in df[:3].iterrows():
+	for index, row in df[:5].iterrows():
 		with st.container(border=True):
 			st.header(row['title'])
 			st.write(row["description"])
@@ -27,7 +25,7 @@ with col3:
 			st.write(f"[Source Code]({row['url']})")
 
 with col4:
-	for index, row in df[3:].iterrows():
+	for index, row in df[5:].iterrows():
 		with st.container(border=True):
 			st.header(row["title"])
 			st.write(row["description"])
--- pages/4_Network and IT Projects.py
+++ pages/4_Network and IT Projects.py
@@ -2,7 +2,14 @@ import streamlit as st
 import pandas
 from pathlib import Path
 
-csv_path = Path(__file__).parent.parent / 'network_and_it_projects_data.csv'
+st.set_page_config(
+	page_title="Network/Automation Projects",
+	page_icon='🐍',
+	layout='wide'
+)
+
+
+csv_path = Path(__file__).parent.parent / 'network_automation_projects_data.csv'
 
 st.header("Network and IT Projects")
 
