b79f29808f380799efc06119533df3a30705f593
osanchez <omar.sanchez008@gmail.com>
2025-07-09 10:45:44 -0400

    Working

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -5,7 +5,7 @@ from pathlib import Path
 csv_path = Path(__file__).parent.parent / 'projects_data.csv'
 
 st.header("My Projects")
-st.write("---")
+# st.write("---")
 
 df = pandas.read_csv(csv_path, sep=";")
 col3, col4 = st.columns(2)
@@ -15,7 +15,7 @@ with col3:
 		with st.container(border=True):
 			st.header(row['title'])
 			st.write(row["description"])
-			st.image("images/" + row["image"])
+			st.image("images/" + row["image"], use_container_width=True)
 			st.write(row['goal'])
 			st.write(f"[Source Code]({row['url']})")
 
@@ -24,6 +24,6 @@ with col4:
 		with st.container(border=True):
 			st.header(row["title"])
 			st.write(row["description"])
-			st.image("images/" + row["image"])
+			st.image("images/" + row["image"], use_container_width=True)
 			st.write(row['goal'])
 			st.write(f"[Source Code]({row['url']})")
