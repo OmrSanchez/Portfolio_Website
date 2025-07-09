5f3e7f7edf3cdac6e448f10ea89a5e7c85725d39
osanchez <omar.sanchez008@gmail.com>
2025-07-09 00:04:17 -0400

    Updating format and layout

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -3,7 +3,7 @@ from send_email import send_email
 import pandas
 from pathlib import Path
 
-csv_path = Path(__file__).parent.parent / 'topics.csv'
+csv_path = Path(__file__).parent.parent / "topics.csv"
 
 df = pandas.read_csv(csv_path)
 
@@ -11,8 +11,7 @@ st.header("Contact Me")
 
 with st.form(key='email_form'):
     user_email = st.text_input("Your email address", placeholder="Enter email")
-    topics = st.selectbox("What topic do you want to discuss?",
-                          df['topic'], index=None)
+    topics = st.selectbox("What topic do you want to discuss?", df['topic'], index=None)
     raw_message = st.text_area("Your message", placeholder="Your message here")
     message = f"""\
 Subject: New email from {user_email}
