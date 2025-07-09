7147a94bcf92686cb4e3b63d96fca3c74b92a4d6
osanchez <omar.sanchez008@gmail.com>
2025-07-09 10:31:32 -0400

    Working

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pandas
 from pathlib import Path
 
-csv_path = Path(__file__).parent.parent / 'data.csv'
+csv_path = Path(__file__).parent.parent / 'projects_data.csv'
 
+st.header("My Projects")
+st.write("---")
 
 df = pandas.read_csv(csv_path, sep=";")
 col3, col4 = st.columns(2)
 
 with col3:
-	for index, row in df[:2].iterrows():
+	for index, row in df[:3].iterrows():
 		with st.container(border=True):
 			st.header(row['title'])
 			st.write(row["description"])
@@ -18,7 +20,7 @@ with col3:
 			st.write(f"[Source Code]({row['url']})")
 
 with col4:
-	for index, row in df[2:].iterrows():
+	for index, row in df[3:].iterrows():
 		with st.container(border=True):
 			st.header(row["title"])
 			st.write(row["description"])
