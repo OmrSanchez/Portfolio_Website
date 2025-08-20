46388227fdaa78c2e6c0b309beb42fe9f83b8aa4
osanchez <omar.sanchez008@gmail.com>
2025-08-20 13:53:54 -0400

    Added: Dual Site 3-Tier Network Lab

--- pages/4_Network Projects.py
+++ pages/4_Network Projects.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 csv_path = Path(__file__).parent.parent / 'network_automation_projects_data.csv'
 
-st.title("Network Design, Troubleshooting, Automation, and IT Projects")
+st.title("Network Projects")
 
 col_count = 1
 net_df = pandas.read_csv(csv_path, sep=";")
